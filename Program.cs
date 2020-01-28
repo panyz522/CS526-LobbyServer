dfc8581acf2fb4a999a8cbeaf411c85ffeaa1345
Yongzhen Pan <panyz522@gmail.com>
2020-01-28 14:14:05 -0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS526_LobbyServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
